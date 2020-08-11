d0cd5b7a020a24fd9321100b60362083de6088ff
FlavScheidt <flavscheidr@gmail.com>
2020-08-11 18:53:59 +0200

    Deployable version

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-const ConvertLib = artifacts.require("MemberApp");
-const MetaCoin = artifacts.require("SuperApp");
+const MemberApp = artifacts.require("MemberApp");
+const SuperApp = artifacts.require("SuperApp");
 
 module.exports = function(deployer) {
-  deployer.deploy(MemberApp);
-  deployer.link(MemberApp, SuperApp);
+  // deployer.deploy(MemberApp);
+  // deployer.link(MemberApp, SuperApp);
   deployer.deploy(SuperApp);
 };
