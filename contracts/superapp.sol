4cf4d2ef4d1ea184b7372a150d9334cb7e4d14be
FlavScheidt <flavscheidr@gmail.com>
2020-08-11 14:47:38 +0200

    Data structures and skeleton functions for the supeapp SC

--- contracts/superapp.sol
+++ contracts/superapp.sol
@@ -1,23 +1,29 @@
-pragma solidity ^0.6.4;
+pragma solidity ^0.7.0;
 
 /*************************
-	 Global Structs
+	 Global Structures
 **************************/
+enum memberStatus { Unset, Active, Inactive }
 struct memberApp
 {
 	bytes16 publicKey;
 	bytes32 [32] encAddress; //Bytes32 is more efficient and cheap than trings because they dont carry the UTF-8 enconding
 	bytes16 enclaveSignature;
 	address memberAppAddress;
-	bool status;
+	address memberAppOwner; //When creating a new member app, that's the caller address
 }
-
 
 contract superApp
 {
-	//List of member apps
-	
+	/*************************
+		List of member apps
+	**************************/
+	//We store only the address of the smart contract and map to its status
 
+
+	/*************************
+		Structs and other variables
+	**************************/	
 
 	/*************************
 		Constructor
@@ -33,13 +39,26 @@ contract superApp
 	//This function receives the information about a memberApp to be add
 	//Creates a new smart contract and returns it`s address
 	//The adress of the contract must be stored here, with its status
-	function NewMember () public returns (address)
+	function newMember () public returns (address)
 	{
+		address owner = msg.sender;
 
 	}
 
 	/*************************
 		Delete Member
 	**************************/
+	function deleteMember () public returns (bool)
+	{
+
+	}
+
+	/*************************
+		Verify membership
+	**************************/
+	function isMember () public returns (bool)
+	{
+
+	}
 
 }
